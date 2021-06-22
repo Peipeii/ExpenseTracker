ef4c3a4620e33d2ec2319463bb57ca13da510fd8
Peipeii <pey728@gmail.com>
2021-06-21 20:37:34 -0700

    feat: remove block comment

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -27,11 +27,4 @@ function setCategories (obj) {
       console.log('mongodb disconnected!')
     })
     .catch(err => console.log(err))
-  // for (const key in recordList.results) {
-  //   const name = recordList.results[key].category
-  //   const code = categoryList.find(ct => ct.name === name).code
-  //   // 將類別名稱對應類別代碼塞進records表裡
-  //   recordList.results[key].categoryIcon = code
-  //   Record.create(recordList.results[key])
-  // }
 }
